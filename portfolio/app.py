703000972e7c781408d7befd09d751ea97925307
Jack Arthur Harrhy <me@jackharrhy.com>
2021-06-19 11:48:14 -0230

    blog posts, listing of them all, bring in markdown, slugs :slug:

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+import os
+import urllib
+from datetime import datetime
+from pathlib import Path
+
+from flask import Flask, abort
 from flask import render_template
 import decouple
 import requests
+import markdown
 
 GITHUB_REPOS_URL = "https://api.github.com/users/jackharrhy/repos?per_page=100"
 CONTACT_FORM_ACTION = decouple.config("CONTACT_FORM_ACTION", default=None)
@@ -43,19 +49,65 @@ for repo in repos:
         )
 
 
+blog_posts = []
+blog_slug_to_post = {}
+
+
+with os.scandir("blog") as it:
+    for entry in it:
+        if entry.name.endswith(".md") and entry.is_file():
+            raw_date, title = entry.name.split("_")
+            title = title.rstrip(".md")
+
+            date = datetime.strptime(raw_date, "%Y-%m-%d")
+
+            post_markdown = Path(entry.path).read_text()
+            html = markdown.markdown(post_markdown)
+
+            slug = "".join(filter(str.isalnum, title))
+            slug = urllib.parse.quote_plus(slug).lower()
+
+            print(slug)
+
+            post = {
+                "date": date,
+                "title": title,
+                "html": html,
+                "slug": slug,
+            }
+
+            blog_posts.append(post)
+            blog_slug_to_post[slug] = post
+
+
 @app.route("/")
-def about_route(name=None):
+def about_route():
     return render_template("about.html")
 
 
 @app.route("/projects/")
-def projects_route(name=None):
+def projects_route():
     return render_template("projects.html", projects=projects)
 
 
 @app.route("/contact/")
-def contact_route(name=None):
+def contact_route():
     return render_template("contact.html", api=CONTACT_FORM_ACTION)
+
+
+@app.route("/blog/")
+def blog_route():
+    return render_template("blog.html", blog_posts=blog_posts)
+
+
+@app.route("/blog/<slug>")
+def blog_post_route(slug):
+    post = blog_slug_to_post.get(slug)
+
+    if post is None:
+        abort(404, description="Post not found")
+
+    return render_template("blog_post.html", post=post)
 
 
 if __name__ == "__main__":
---
+++ portfolio/blog/2020-06-19_First entry.md
@@ -0,0 +1,51 @@
+# this is my first blog post!
+
+## is is written in markdown
+
+i'm a fan of markdown!
+
+here is a sample of markdown, so you can see what it looks like
+in the text file, but also how it renders on the website!
+
+---
+
+**This is bold text**
+
+**This is bold text**
+
+_This is italic text_
+
+_This is italic text_
+
+~~Strikethrough~~
+
+---
+
+## Lists
+
+Unordered
+
+-   Create a list by starting a line with `+`, `-`, or `*`
+-   Sub-lists are made by indenting 2 spaces:
+    -   Marker character change forces new list start:
+        -   Ac tristique libero volutpat at
+        *   Facilisis in pretium nisl aliquet
+        -   Nulla volutpat aliquam velit
+-   Very easy!
+
+---
+
+Inline `code`
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+Block code "fences"
+
+```
+Sample text here...
+```
